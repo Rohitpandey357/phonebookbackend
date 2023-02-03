82546994ef5e513430ea4bf315254c8202885654
Rohit Pandey <rohitpandey357@gmail.com>
2023-02-03 19:58:53 +0530

    Completed exercise 3.3

--- index.js
+++ index.js
@@ -39,6 +39,16 @@ app.get('/info', (request, response) => {
   `);
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = parseInt(request.params.id);
+  const person = persons.find(p => p.id === id);
+  if(person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
