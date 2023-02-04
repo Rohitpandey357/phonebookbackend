3d7085ef3d4097fdee80e075d75c603c265c4731
Rohit Pandey <rohitpandey357@gmail.com>
2023-02-04 12:31:04 +0530

    Added the production frontend build

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
 app.use(express.json());
 app.use(morgan('tiny'))
+app.use(cors());
+app.use(express.static('build'));
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms -' 
@@ -85,7 +88,8 @@ app.post('/api/persons', (request, response) => {
   response.status(201).json(person);
 });
 
-const PORT = 3001
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
